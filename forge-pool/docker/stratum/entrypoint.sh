81fd779912737e046238562535c6772851a62a62
VoidHash-Crypto <voidhash@outlook.com>
2026-07-01 18:40:35 +0100

    Fork umbrel-app-store for VoidCoin
    - Remove BCH2/forge-pool app entries
    - Add voidcoin-pool app entry with VoidCoin branding
    - Update umbrel-app-store.yml for VoidHash-Crypto org

--- forge-pool/docker/stratum/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Generate config from template with environment variables
-envsubst < /etc/forge/config.template.yaml > /etc/forge/config.yaml
-
-# Start stratum server
-exec /usr/local/bin/stratum -config /etc/forge/config.yaml
